79c7d2a28748c3d3a800eedc3c0429a609a438f6
derique96 <fowlkes.derique@gmail.com>
2023-05-07 21:10:34 -0400

    application successful

--- Develop/server.js
+++ Develop/server.js
@@ -1,66 +1,57 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const notesData = require('./db/db.json');
-const PORT = 3001;
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const notesData = require("./db/db.json");
+const PORT = process.env.PORT || 3001;
 const app = express();
-const uuid = require('./helpers/uuid');
+const uuid = require("./helpers/uuid");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    res.json(JSON.parse(data));
+  });
 });
 
-app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.post("/api/notes", (req, res) => {
+  console.info`${req.method} request received to add title and text`;
+
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNotes = {
+      id: uuid(),
+      title,
+      text,
+    };
+
+    fs.readFile("./db/db.json", (err, data) => {
+      const parsedNotes = JSON.parse(data);
+      parsedNotes.push(newNotes);
+
+      fs.writeFile(
+        "./db/db.json",
+        JSON.stringify(parsedNotes, null, 4),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : res.json(newNotes)
+      );
+    });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.get('/api/notes', (req, res) => { 
-    res.json(`${req.method} request received to add a title and text`);
-    console.log(`${req.method} request received to add a title and text`);
-});
-
-
-app.post('/api/notes', (req, res) => {
-    console.info`${req.method} request received to add title and text`
-
-    const {title, text} = req.body;
-    if (title && text){
-        const newNotes = {
-            title,
-            text,
-            note_id: uuid(),
-        };
-
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if(err) {
-                console.error(err);
-            } else {
-                const parsedNotes = JSON.parse(data);
-                parsedNotes.push(newNotes);
-
-                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4),
-                (writeErr) =>
-                writeErr
-                    ? console.error(writeErr)
-                    : console.info('You have submitted a new note!')
-                );
-            };
-        });
-        
-    };
-});
-
-
-
-
-
-
-
 app.listen(PORT, () => {
-    console.log(`Serving static asset routes on port ${PORT}!`)
+  console.log(`Serving static asset routes on port ${PORT}!`);
 });
